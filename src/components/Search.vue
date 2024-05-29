d610d91ba0e2b652511847dff780703b2530cfb4
Vi <vetttaa@mail.ru>
2024-05-29 14:52:15 +0300

    feat: data search and sorting

---
+++ src/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+    <form @submit.prevent="searchStore.getMovies(searchMovie)">
+        <input type="text" class="search-input" placeholder="Input movie name" v-model="searchMovie">
+    </form>
+    <Loader v-if="searchStore.loader" />
+    <div v-else>
+        <Movie v-for="movie of searchStore.movies" :key="movie.id" :movie="movie" :isSearch="true"/>
+    </div>
+</template>
+
+<script setup>
+import Loader from '../components/Loader.vue'
+import Movie from '../components/Movie.vue'
+import { ref } from 'vue'
+import { useSearchStore } from '../stores/SearchStore.js'
+
+const searchMovie = ref('')
+const searchStore = useSearchStore()
+</script>
+
+<style lang="css" scoped>
+.search-input {
+    width: 100%;
+    height: 40px;
+    padding: 0 10px;
+    border-radius: 10px;
+    border: 1px solid #efefef;
+    margin-bottom: 20px;
+    border-radius: 10px;
+}
+</style>
